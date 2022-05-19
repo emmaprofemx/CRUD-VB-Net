10e0ede7f04d41311a1734a67c29c9d567911787
EmmanuelRuiz5307 <emmaprofemx@gmail.com>
2022-05-18 20:08:48 -0500

    Creacion del formulario con los objetos listos para ser modificados.

--- capaPresentacion/frEmpleados.Designer.vb
+++ capaPresentacion/frEmpleados.Designer.vb
@@ -22,17 +22,151 @@ Partial Class frEmpleados
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtId = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lnkFoto = New System.Windows.Forms.LinkLabel()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.openFoto = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.txtId, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Form1
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(38, 310)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 0
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(17, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Id"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(91, 77)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(135, 23)
+        Me.txtNombre.TabIndex = 2
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(91, 116)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(135, 23)
+        Me.txtApellido.TabIndex = 3
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(91, 44)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(120, 23)
+        Me.txtId.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Apellido"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.PictureBox1.Location = New System.Drawing.Point(91, 161)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(108, 97)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'lnkFoto
+        '
+        Me.lnkFoto.AutoSize = True
+        Me.lnkFoto.Location = New System.Drawing.Point(91, 265)
+        Me.lnkFoto.Name = "lnkFoto"
+        Me.lnkFoto.Size = New System.Drawing.Size(67, 15)
+        Me.lnkFoto.TabIndex = 8
+        Me.lnkFoto.TabStop = True
+        Me.lnkFoto.Text = "Seleccionar"
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(151, 310)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 9
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(289, 310)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 10
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'frEmpleados
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 467)
-        Me.Name = "Form1"
+        Me.ClientSize = New System.Drawing.Size(448, 361)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.lnkFoto)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Name = "frEmpleados"
         Me.Text = "Form1"
+        CType(Me.txtId, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtId As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lnkFoto As LinkLabel
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents openFoto As OpenFileDialog
 End Class
--- capaPresentacion/frEmpleados.vb
+++ capaPresentacion/frEmpleados.vb
@@ -1,3 +1,41 @@
 ﻿Public Class frEmpleados
 
+    Private Sub frEmpleados_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub frEmpleados_Load_1(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub frEmpleados_Load_2(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnNuevo_Click_1(sender As Object, e As EventArgs) Handles btnNuevo.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtNombre.TextChanged
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles txtId.ValueChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+    End Sub
 End Class
